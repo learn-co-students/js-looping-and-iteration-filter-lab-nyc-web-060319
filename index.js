a5266628f6384472ba074d649525516d97712ed7
Alexander Riccio <test35965+learn@gmail.com>
2019-07-12 17:31:57 -0400

    solve lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+'use strict'
 // Code your solution in this file
+function findMatching(drivers, string) {
+    return drivers.filter(function(drv){return drv.toUpperCase() == string.toUpperCase();});
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(drv){return drv.substring(0, letters.length) === letters;});
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(drv){return drv.name === name;});
+}
