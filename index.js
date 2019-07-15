157ab985f56c6151b77877f02e7448c20c877bbf
eschechter <schechter.evan@gmail.com>
2019-07-14 21:30:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, name){
+    return array.filter(function(ele){return ele.toLowerCase() === name.toLowerCase()});
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(ele){return ele.slice(0,2) === string.slice(0,2)});
+}
+
+function matchName(array, name) {
+    return array.filter(function(obj){return obj.name === name});
+}
